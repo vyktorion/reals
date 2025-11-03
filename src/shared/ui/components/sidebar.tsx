0cb7f2d436af93e45d4bc89c2a37c980c78ef7cd
vyktorion <vyktorion@gmail.com>
2025-11-03 20:15:16 +0200

    w1

--- src/shared/ui/components/sidebar.tsx
+++ src/shared/ui/components/sidebar.tsx
@@ -5,7 +5,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
 
-import { useIsMobile } from "./use-mobile";
+import { useIsMobile } from "../hooks/use-mobile";
 import { cn } from "./utils";
 import { Button } from "./button";
 import { Input } from "./input";
